1b592542bd5bb38e01bbfd1a8381ba8f4c746cc0
birik41 <biriq42@gmail.com>
2023-02-23 09:20:59 -0800

    2-mul.c

--- 0x04-more_functions_nested_loops/2-mul.c
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -4,7 +4,7 @@
  * mul - Multiplication two integers
  * @a: The first number to multiply
  * @b: The second the number to multiply with first number
- * Return the result of multiplication between a param and b param
+ * Return: Always 0
  */
 int mul(int a, int b)
 {
