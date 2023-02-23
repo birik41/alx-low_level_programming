6735a6ac424d81b43e4e2dbac8da0fa13f01a949
birik41 <biriq42@gmail.com>
2023-02-23 09:29:20 -0800

    3-print_numbers.c

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,16 @@
+#include "main"
+
+/**
+ * print_numbers - Print the numbers since 0 up to 9
+ * Return: Always 0
+ */
+
+void print_numbers(void)
+{
+	int c;
+	for (c =0; c<= 9; c++)
+	{
+		_putchar(c + '0');
+	}
+	_putchar('\n');
+}
