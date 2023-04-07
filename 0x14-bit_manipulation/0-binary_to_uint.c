698c214b46d1446a8f6a759ae26cf6ed5161eac9
Hassan Birik Mohamed <119738756+birik41@users.noreply.github.com>
2023-04-07 17:04:34 +0300

    Update 0-binary_to_uint.c

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -9,7 +9,7 @@
 unsigned int binary_to_uint(const char *b)
 {
 	int i;
-	unsigned int xasan = 0;
+	unsigned int dec_var = 0;
 
 	if (!b)
 		return (0);
@@ -17,7 +17,7 @@ unsigned int binary_to_uint(const char *b)
 	{
 		if (b[i] < '0' || b[i] > '1')
 			return (0);
-		xasan = 2 * xasan + (b[i] - '0');
+		dec_var = 2 * dec_var + (b[i] - '0');
 	}
-	return (xasan);
+	return (dec_var);
 }
