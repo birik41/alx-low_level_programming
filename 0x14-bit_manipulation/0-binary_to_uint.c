94327e0a73bf2aad4817d9335470e5f563cef1ce
birik41 <biriq42@gmail.com>
2023-04-08 07:35:58 -0700

    0-binary_to_uint.c

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -9,15 +9,17 @@
 unsigned int binary_to_uint(const char *b)
 {
 	int i;
-	unsigned int dec_var = 0;
+	unsigned int dec_val = 0;
 
 	if (!b)
 		return (0);
+
 	for (i = 0; b[i]; i++)
 	{
 		if (b[i] < '0' || b[i] > '1')
 			return (0);
-		dec_var = 2 * dec_var + (b[i] - '0');
+		dec_val = 2 * dec_val + (b[i] - '0');
 	}
-	return (dec_var);
+
+	return (dec_val);
 }
